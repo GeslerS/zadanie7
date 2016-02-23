278de259a5fe0302ed30a21824be312c741a0b9c
szymon <szymon.gesler@yahoo.com>
2016-02-23 17:31:19 +0100

    Poprawki

--- src/test/java/pl/sagiton/controller/helloControllerTest.java
+++
@@ -1,7 +0,0 @@
-package pl.sagiton.controller;
-
-/**
- * Created by szymon on 22.02.16.
- */
-public class helloControllerTest {
-}
--- src/test/java/pl/sagiton/service/MyUserDetailsServiceTest.java
+++ src/test/java/pl/sagiton/service/MyUserDetailsServiceTest.java
@@ -6,10 +6,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.powermock.api.mockito.PowerMockito;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import pl.sagiton.repository.UserService;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -18,17 +28,31 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class MyUserDetailsServiceTest {
 
-    @InjectMocks
-    UserService userService = new UserService();
+    @Mock
+    UserService userService = mock(UserService.class);
 
+    @InjectMocks
     private MyUserDetailsService myUserDetailsService = new MyUserDetailsService();
+
+    private  Map<String, String> userMap = new HashMap<String, String>();
 
 
     @Test(expected = UsernameNotFoundException.class)
-    public void UsernameNotFoundException(){
+    public void usernameNotFoundException(){
         when(userService.getUserDetails(anyString())).thenReturn(null);
         myUserDetailsService.loadUserByUsername("Szymon");
     }
 
+    @Test
+    public void userDetailsTest(){
 
+        userMap.put("username", "Johny");
+        userMap.put("password", "Johny123");
+
+      when(userService.getUserDetails(anyString())).thenReturn(userMap);
+      UserDetails user = myUserDetailsService.loadUserByUsername("Johny");
+      assertEquals(user.getUsername(), "Johny");
+      assertEquals(user.getPassword(), "Johny123");
+
+    }
 }
